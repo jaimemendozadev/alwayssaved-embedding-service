14ccead15b5320544c1445a40d7bcc4cf03c3178
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-04-27 10:36:38 -0700

    Scaffold /qdrant helper functions

--- services/qdrant/main.py
+++ services/qdrant/main.py
@@ -1,5 +1,26 @@
+import os
+
 from qdrant_client import QdrantClient
+from sentence_transformers import SentenceTransformer
 
 
 def get_collection(Q_Client: QdrantClient):
     pass
+
+
+def get_embedd_model() -> SentenceTransformer:
+    embedd_model_name = os.getenv("EMBEDDING_MODEL", "multi-qa-MiniLM-L6-cos-v1")
+    # Load model
+    embedding_model = SentenceTransformer(embedd_model_name)
+
+    return embedding_model
+
+
+def get_qdrant_client() -> QdrantClient:
+
+    # Connect to Qdrant
+    qdrant = QdrantClient(
+        url="https://your-qdrant-cloud-url", api_key="your-qdrant-api-key"
+    )
+
+    return qdrant
