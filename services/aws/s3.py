afa53e0cd3dfdb1374080ab0e7207bdacb86642e
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-04-27 17:40:43 -0700

    Add extract_text_from_s3_bytes function to s3 file

--- services/aws/s3.py
+++ services/aws/s3.py
@@ -1,6 +1,25 @@
+from io import BytesIO
 from urllib.parse import urlparse
 
 import boto3
+import pdfplumber
+from bs4 import BeautifulSoup
+
+
+def extract_text_from_s3_bytes(file_bytes: bytes, file_extension: str) -> str:
+    if file_extension == ".txt":
+        return file_bytes.decode("utf-8")  # Simple text
+    elif file_extension == ".pdf":
+        text = ""
+        with pdfplumber.open(BytesIO(file_bytes)) as pdf:
+            for page in pdf.pages:
+                text += page.extract_text() or ""
+        return text
+    elif file_extension == ".html":
+        soup = BeautifulSoup(file_bytes, "html.parser")
+        return soup.get_text()
+    else:
+        raise ValueError(f"Unsupported file extension: {file_extension}")
 
 
 def download_file_from_s3(s3_client: boto3.client, s3_url: str) -> bytes:
