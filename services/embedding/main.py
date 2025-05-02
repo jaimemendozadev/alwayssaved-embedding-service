7727543ea8641eca01f96a3de1bab2568182682d
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-05-02 11:58:18 -0700

    Add error handling to embed_and_upload

--- services/embedding/main.py
+++ services/embedding/main.py
@@ -4,9 +4,9 @@ import uuid
 import boto3
 from qdrant_client import QdrantClient, models
 from sentence_transformers import SentenceTransformer
-from utils.types.main import SQSPayload
 
 from services.aws.s3 import download_file_from_s3, extract_text_from_s3_bytes
+from services.utils.types.main import SQSPayload
 
 
 def get_embedd_model() -> SentenceTransformer:
@@ -36,38 +36,51 @@ def embed_and_upload(
     s3_client: boto3.client,
     sqs_payload: SQSPayload,
 ):
-    note_id = sqs_payload.get("note_id")
-    transcript_url = sqs_payload.get("transcript_url")
-    user_id = sqs_payload.get("user_id")
+    try:
+        note_id = sqs_payload.get("note_id", None)
+        transcript_url = sqs_payload.get("transcript_url", None)
+        user_id = sqs_payload.get("user_id", None)
 
-    file_bytes = download_file_from_s3(s3_client, transcript_url)
-
-    _, file_extension = os.path.splitext(transcript_url)
-
-    file_extension = file_extension.lower()
-
-    full_text = extract_text_from_s3_bytes(file_bytes, file_extension)
-
-    chunks = chunk_text(full_text)
-
-    vectors = embedding_model.encode(chunks, normalize_embeddings=True)
-
-    points = []
-
-    for idx, (chunked_text, vector) in enumerate(zip(chunks, vectors)):
-        points.append(
-            models.PointStruct(
-                id=str(uuid.uuid4()),  # unique ID per chunk
-                vector=vector.tolist(),
-                payload={
-                    "_id": str(note_id),
-                    "user_id": user_id,
-                    "source_transcript_url": transcript_url,
-                    "original_chunk_text": chunked_text,
-                },
+        if note_id is None or transcript_url is None or user_id is None:
+            raise ValueError(
+                f"SQS Message Payload from Extractor Service is missing note_id: {note_id}, user_id: {user_id}, or transcript_url: {transcript_url} \n"
             )
-        )
 
-    qdrant_client.upsert(collection_name="notecasts-transcripts", points=points)
+        file_bytes = download_file_from_s3(s3_client, transcript_url)
 
-    print(f"✅ Uploaded {len(points)} chunks to Qdrant!")
+        if file_bytes is None:
+            raise ValueError(
+                f"Could not get the requested s3 file: {transcript_url} \n"
+            )
+
+        _, file_extension = os.path.splitext(transcript_url)
+
+        file_extension = file_extension.lower()
+
+        full_text = extract_text_from_s3_bytes(file_bytes, file_extension)
+
+        chunks = chunk_text(full_text)
+
+        vectors = embedding_model.encode(chunks, normalize_embeddings=True)
+
+        points = []
+
+        for idx, (chunked_text, vector) in enumerate(zip(chunks, vectors)):
+            points.append(
+                models.PointStruct(
+                    id=str(uuid.uuid4()),  # unique ID per chunk
+                    vector=vector.tolist(),
+                    payload={
+                        "_id": str(note_id),
+                        "user_id": user_id,
+                        "source_transcript_url": transcript_url,
+                        "original_chunk_text": chunked_text,
+                    },
+                )
+            )
+        qdrant_client.upsert(collection_name="notecasts-transcripts", points=points)
+
+        print(f"✅ Uploaded {len(points)} chunks to Qdrant!")
+
+    except ValueError as e:
+        print(f"❌ Value Error: {e}")
