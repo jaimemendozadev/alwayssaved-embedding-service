658a8dfc6691540570a665de145d7a2c1da0cb04
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-06-22 16:57:34 -0700

    Fix ThreadPoolExecutor and thread_executor_worker issues
    Was not invoking PoolExecutor in code & changed
    thread_executor_worker because executor.map
    can't run async functions directly. Also
    comment out ses function for now.

--- service.py
+++ service.py
@@ -1,8 +1,9 @@
+import asyncio
 import json
 import os
 import time
 import traceback
-from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 from typing import TYPE_CHECKING
 
 import boto3
@@ -46,9 +47,9 @@ def executor_worker(json_payload: str):
     return embed_and_upload(payload_dict)
 
 
-async def thread_executor_worker(embed_status: EmbedStatus):
+def thread_executor_worker(embed_status: EmbedStatus):
     user_id = embed_status["user_id"]
-    await send_user_email_notification(ses_client, mongo_client, user_id)
+    asyncio.run(send_user_email_notification(ses_client, mongo_client, user_id))
 
 
 def run_service():
@@ -124,8 +125,8 @@ def run_service():
             delete_embedding_sqs_message(successful_results)
 
             # 4) Fire an SES Email For Each Successful Embedd/Upload Message.
-            with ThreadPoolExecutor as executor:
-                list(executor.map(thread_executor_worker, successful_results))
+            # with ThreadPoolExecutor() as executor:
+            #     list(executor.map(thread_executor_worker, successful_results))
 
         except ValueError as e:
             print(f"ValueError in run_service function: {e}")
